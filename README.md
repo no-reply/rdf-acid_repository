e4da933ade4511915c246a48914675133186dc26
Tom Johnson <tom@dp.la>
2016-01-15 19:10:25 -0800

    Add README.md
    Also removing commented out tests.

---
+++ README.md
@@ -0,0 +1,12 @@
+RDF::AcidRepository
+===================
+
+This is an in-development in-memory `RDF::Repository` using functional
+datastructures.
+
+You maybe can't see the point of this, yet. It's work in progress;
+taking advantage of the benefits of the functional data structures is
+next on the list. :)
+
+When work has progressed a bit further, this will likely move to a new
+home.
--- spec/lib/rdf/acid_repository_spec.rb
+++ spec/lib/rdf/acid_repository_spec.rb
@@ -6,28 +6,4 @@ describe RDF::AcidRepository do
   
   let(:repository) { RDF::AcidRepository.new }
   it_behaves_like 'an RDF::Repository'
-
-  # describe '#insert_statement' do
-  #   it do
-  #     subject.insert_statement(RDF::Statement(:s, RDF.type, :o)) 
-  #     subject.insert_statement(RDF::Statement(:s, RDF.type, :q)) 
-  #     subject.insert_statement(RDF::Statement(:s, RDF.type, :r))
-  #     subject.count
-  #     subject.insert_statement(RDF::Statement(:s, RDF::RDFS.label, :q)) 
-  #     subject.count
-  #     subject.insert_statement(RDF::Statement(:t, RDF::RDFS.label, :q)) 
-
-  #     subject.count
-
-  #     subject.delete_statement(RDF::Statement(:s, RDF.type, :q)) 
-  #     subject.delete_statement(RDF::Statement(:s, RDF.type, :o)) 
-  #     subject.count
-  #     subject.delete_statement(RDF::Statement(:s, RDF.type, :r))
-  #     subject.delete_statement(RDF::Statement(:s, RDF::RDFS.label, :q)) 
-  #     subject.count
-  #     subject.delete_statement(RDF::Statement(:t, RDF::RDFS.label, :q)) 
-  #     subject.count
-      
-  #   end
-  # end
 end
